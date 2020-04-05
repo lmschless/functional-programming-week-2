cdc0739978df3709cd23b0dc55324219b4974424
Luke <lmschless@gmail.com>
2020-04-05 09:49:06 -0700

    working on second example

--- js-code.js
+++ js-code.js
@@ -1,3 +1,17 @@
 require('./js-code.js');
 
 // new linter can see that let is not allowed
+const canEat = function(creature) {
+	const obj = {
+		eat: function(food) {
+			return `The ${creature} eats the ${food}.`;
+		}
+	};
+	return obj;
+};
+
+const cat = canEat('cat');
+cat.eat('salmon');
+
+const salmon = canEat('salmon');
+salmon.eat('insects');
