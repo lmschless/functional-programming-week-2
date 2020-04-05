ddb8eb09dbfeade4acfcb7a96ccd1243b4af0157
Luke <lmschless@gmail.com>
2020-04-05 09:53:25 -0700

    .

--- js-code.js
+++ js-code.js
@@ -10,6 +10,20 @@ const canEat = function(creature) {
 	return obj;
 };
 
+// use two separate methods, one for eat and one for sleep
+// keeps code modular and more reusable.
+const canSleep = function(creature) {
+	const obj = {
+		sleep: function() {
+			return `The ${creature} sleeps.`;
+		}
+	};
+	return obj;
+};
+
+// Make a function factory to assign both
+// methods to a single cat object.
+
 const cat = canEat('cat');
 cat.eat('salmon');
 
