f09e73556c8ca68cac3edd5574c9f3950e03be89
Luke <lmschless@gmail.com>
2020-04-07 20:59:57 -0700

    last commit tonight, working composition problems.

--- composition.js
+++ composition.js
@@ -11,3 +11,12 @@ console.log(painter1.paints());
 
 const painter2 = paint('red');
 console.log(painter2.paints());
+
+const sound = (noise) => ({
+	sound: () => {
+		return noise;
+	}
+});
+
+const faucet = sound('drip drip drip');
+console.log(faucet.sound());
